27c0c7afde296f15b73a7ed2e28a1a97579e3d52
ivanovdale <dania-spb@mail.ru>
2024-05-06 22:54:20 +0300

    implement users model

---
+++ iOS12-HW22-Daniil-Ivanov/Sources/Users/Model/Gender.swift
@@ -0,0 +1,12 @@
+//
+//  Gender.swift
+//  iOS12-HW22-Daniil-Ivanov
+//
+//  Created by Daniil (work) on 29.04.2024.
+//
+
+import Foundation
+
+enum Gender: String, CaseIterable {
+    case male, female, other
+}
