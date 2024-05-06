e64527813c3a9e0571980e8c585c227aa27caabd
ivanovdale <dania-spb@mail.ru>
2024-05-06 22:56:17 +0300

    implement router

---
+++ iOS12-HW22-Daniil-Ivanov/Sources/App/Router.swift
@@ -0,0 +1,16 @@
+//
+//  Router.swift
+//  iOS12-HW22-Daniil-Ivanov
+//
+//  Created by Daniil (work) on 26.04.2024.
+//
+
+import UIKit
+
+final class Router {
+    private var controller: UIViewController?
+
+    func setRootController(controller: UIViewController) {
+        self.controller = controller
+    }
+}
